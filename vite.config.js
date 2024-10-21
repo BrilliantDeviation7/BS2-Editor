48fc19497e00a0f54d5aad00b778bd5dcee0b369
BrilliantDeviation7 <54754524+BrilliantDeviation7@users.noreply.github.com>
2024-10-21 11:21:04 -0400

    Create PWA

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,61 @@
 import { defineConfig } from 'vite';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svelte()],
+	plugins: [
+		svelte(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			devOptions: {
+				enabled: true
+			},
+			manifest: {
+				name: 'BS2 Editor',
+				short_name: 'Editor',
+				description: 'Edit PBASIC for BS2',
+				theme_color: '#ffffff',
+				icons: [
+					{
+						src: 'pwa-64x64.png',
+						sizes: '64x64',
+						type: 'image/png'
+					},
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any'
+					},
+					{
+						src: 'maskable-icon-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'maskable'
+					}
+				]
+			}
+		})
+	],
 	resolve: {
 		alias: {
 			$lib: path.resolve('./src/lib')
 		}
 	},
 	optimizeDeps: {
-		exclude: ['codemirror', '@lezer/highlight', '@codemirror/language', '@codemirror/view', '@codemirror/commands']
+		exclude: [
+			'codemirror',
+			'@lezer/highlight',
+			'@codemirror/language',
+			'@codemirror/view',
+			'@codemirror/commands'
+		]
 	}
 });
