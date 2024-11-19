7f201682a02fb02f52564f226d7560186bea908a
BrilliantDeviation7 <54754524+BrilliantDeviation7@users.noreply.github.com>
2024-11-18 21:33:34 -0500

    Add autocomplete

--- src/lib/editor/extensions.js
+++ src/lib/editor/extensions.js
@@ -14,6 +14,46 @@ import { StreamLanguage } from '@codemirror/language';
 import { syntaxHighlighting, HighlightStyle } from '@codemirror/language';
 import { Tag, tags } from '@lezer/highlight';
 
+import { snippetCompletion, autocompletion } from '@codemirror/autocomplete';
+
+function customCompletions(context) {
+	let word = context.matchBefore(/\w*/);
+	if (word.from === word.to && !context.explicit) {
+		return null;
+	}
+
+	return {
+		from: word.from,
+		options: [
+			snippetCompletion('IF ${} THEN\n\t${}\nENDIF', {
+				label: 'if',
+				type: 'text',
+				info: 'IF ... THEN\n\nENDIF'
+			}),
+			snippetCompletion('IF ${} THEN\n\t${}\nELSE\n\t${}\nENDIF', {
+				label: 'if_else',
+				type: 'text',
+				info: 'IF ... THEN\n\nELSE\n\nENDIF'
+			}),
+			snippetCompletion('IF ${} THEN\n\t${}\nELSEIF\n\t${}\nELSE\n\t${}\nENDIF', {
+				label: 'if_elseif_else',
+				type: 'text',
+				info: 'IF ... THEN\n\nELSEIF ... THEN\n\nELSE\n\nENDIF'
+			}),
+			snippetCompletion('FOR ${} = ${} to ${}\n\t${}\nNEXT', {
+				label: 'for',
+				type: 'text',
+				detail: 'FOR...NEXT'
+			}),
+			snippetCompletion('DO\n\t${}\nLOOP', {
+				label: 'do',
+				type: 'text',
+				detail: 'DO...LOOP'
+			})
+		]
+	};
+}
+
 // https://codemirror.net/examples/styling/
 let theme = EditorView.theme(
 	{
@@ -21,7 +61,6 @@ let theme = EditorView.theme(
 			width: '100%',
 			maxWidth: '100%',
 			height: '100%',
-			color: '#DCDCAA',
 			backgroundColor: '#1f1f1f'
 		},
 		'.cm-selectionBackground, ::selection': {
@@ -39,10 +78,6 @@ let theme = EditorView.theme(
 );
 
 // https://discuss.codemirror.net/t/codemirror-6-stream-syntax-with-custom-tags/2578/7
-// const customTags = {
-// 	etInstruction: Tag.define(),
-// 	etVariable: Tag.define()
-// };
 
 const styleTags = [
 	'etDirective',
@@ -64,24 +99,18 @@ Object.assign(tags, customTags);
 // also I shoudl have used tags imported from lezer/highlight, not created let tags = {}
 
 const customHighlightStyle = HighlightStyle.define([
-	// { tag: tags['etInstruction'], color: '#5C6E74' },
-	// { tag: tags['etVariable'], color: '#DD4A68' },
 	{ tag: tags.etDirective, color: '#008080' },
 	{ tag: tags.etTargetModule, color: '#008080' },
-	{ tag: tags.etIOFormatter, color: '#2781ff' },
-	{ tag: tags.etVariable, color: '#217edf' },
-	{ tag: tags.etConstant, color: '#9CDCFE' },
+	{ tag: tags.etIOFormatter, color: '#fbbf24' },
+	{ tag: tags.etVariable, color: '#4ec9b0' },
+	{ tag: tags.etConstant, color: '#2781ff' },
 	{ tag: tags.etCCDirective, color: 'rgb(255, 85, 85)' },
 	{ tag: tags.etInstruction, color: '#C586C0' },
-	// { tag: tags.number, color: '#b5cea8' },
-	// { tag: tags.variableName, color: '#4ec9b0' },
-	{ tag: tags.comment, color: '#6a9955' }
+	{ tag: tags.number, color: '#b5cea8' },
+	{ tag: tags.variableName, color: '#9cdcfe' },
+	{ tag: tags.comment, color: '#6a9955' },
+	{ tag: tags.string, color: '#ce9178' }
 ]);
-
-// const myHighlightStyle = HighlightStyle.define([
-// 	{ tag: 'etInstruction', color: 'red' }
-// 	// { tag: tags.etInstruction, color: '#f5d', fontStyle: 'italic' }
-// ]);
 
 export const extensions = [
 	basicSetup,
@@ -95,5 +124,8 @@ export const extensions = [
 			unsavedChanges.set(true);
 			code.set(update.state.doc.toString());
 		}
+	}),
+	autocompletion({
+		override: [customCompletions]
 	})
 ];
